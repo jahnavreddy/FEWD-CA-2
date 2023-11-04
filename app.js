2e0b844755f8745f8a3790dace0e50f4b636bde7
jahnavreddy <144227880+jahnavreddy@users.noreply.github.com>
2023-11-04 21:14:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+var blockSize = 25;
+var rows = 20;
+var columns = 20;
+var snakeboard;
+var context; 
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+var velocityX = 0;
+var velocityY = 0;
+var snakeBody = [];
+var foodX;
+var foodY;  
+var gameOver = false;
+
+window.onload = function() {
+    snakeboard = document.getElementById("snakeboard");
+    snakeboard.height = rows * blockSize;
+    snakeboard.width = columns * blockSize;
+    context = snakeboard.getContext("2d"); 
+
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    setInterval(update, 100);
+}
+
+function placeFood() {
+  foodX = Math.floor(Math.random() * columns) * blockSize;
+  foodY = Math.floor(Math.random() * rows) * blockSize;
+}
+
+function update() {
+    if (gameOver) {
+        return;
+    }
+
+    context.fillStyle="black";
+    context.fillRect(0, 0, snakeboard.width, snakeboard.height);
+
+    context.fillStyle="red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
+        placeFood();
+    }
+
+    for (let i = snakeBody.length-1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i-1];
+    }    
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    context.fillStyle="lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    if (snakeX < 0 || snakeX > columns*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+        gameOver = true;
+        alert("Game Over");
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * columns) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
