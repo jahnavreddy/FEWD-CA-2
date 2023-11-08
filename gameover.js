c6a10edb50913eae3a6032145e0689e188855f59
jahnavreddy <jahnavredy12@gmail.com>
2023-11-08 18:08:33 +0530

    final output

--- gameover.js
+++ gameover.js
@@ -1,7 +1,20 @@
 var  scoreUpdate = document.getElementById("score-board")
 console.log(localStorage.getItem("score"));
 console.log(scoreUpdate);
+var score = localStorage.getItem("score") ;
+var phraseText = document.getElementById("phrase")
 scoreUpdate.innerText = localStorage.getItem("score");
 
-
+if (score>0 && score < 10){
+  phraseText.innerHTML = "GOOD JOB"
+}
+else if(score>9 && score < 20){
+  phraseText.innerHTML = "EXCELLENT"
+}
+else if(score>19 && score < 30){
+  phraseText.innerHTML = "GREAT"
+}
+else if (score>29){
+  phraseText.innerHTML = "MASTERY"
+}
 
